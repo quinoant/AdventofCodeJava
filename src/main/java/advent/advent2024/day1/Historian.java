cb94cf1d2aa1180000df6b277cfd2581b76da237
Anthony Quinones <quinoant@gmail.com>
2024-12-01 13:08:14 -0600

    part one

--- src/main/java/advent/advent2024/day1/Historian.java
+++ src/main/java/advent/advent2024/day1/Historian.java
@@ -1,11 +1,36 @@
 package main.java.advent.advent2024.day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Historian{
-    public static void main(String args[]){
-        System.out.println("x");
+    public static void main(String args[])throws FileNotFoundException {
+        File file = new File("/Users/quinoant/Downloads/AdventofCodeJava/src/main/java/advent/advent2024/day1/input.txt");
+        Scanner sc = new Scanner(file);
+        System.out.println(checker(sc));
     }
 
-    public String checker(){
-        return "hi hole";
+    public static Integer checker(Scanner sc){
+        int listSize = 1000;
+        int[] listOne = new int[listSize];
+        int[] listTwo = new int[listSize];
+        int i = 0;
+        String[] currLine = new String[2];
+        while(sc.hasNextLine()){
+            currLine = sc.nextLine().split("   ");
+            listOne[i] = Integer.parseInt(currLine[0]);
+            listTwo[i] = Integer.parseInt(currLine[1]);
+            //System.out.println(listOne[i] + " : " + listTwo[i]);
+            i++;
+        }
+        Arrays.sort(listOne);
+        Arrays.sort(listTwo);
+        int toReturn = 0;
+        for(int j = 0; j < listSize; j++){
+            toReturn += Math.abs(listOne[j] - listTwo[j]);
+        }
+        return toReturn;
     }
 }
--- src/test/java/advent/advent2024/day1/HistorianTester.java
+++ src/test/java/advent/advent2024/day1/HistorianTester.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 import main.java.advent.advent2024.day1.Historian;
 
 public class HistorianTester {
-    @Test
-    void checker(){
-        Historian historian = new Historian();
-        assertEquals(historian.checker(),"hi hole");
-    }
+    // @Test
+    // void checker(){
+    //     Historian historian = new Historian();
+    //     assertEquals(historian.checker(),"hi hole");
+    // }
 }
