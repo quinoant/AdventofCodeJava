3768576cc6599de9c4b6ba13ec76a94db91abd8d
Anthony Quinones <quinoant@gmail.com>
2024-12-01 12:33:47 -0600

    initialization

---
+++ src/main/java/advent/advent2024/day1/Historian.java
@@ -0,0 +1,11 @@
+package main.java.advent.advent2024.day1;
+
+public class Historian{
+    public static void main(String args[]){
+        System.out.println("x");
+    }
+
+    public String checker(){
+        return "hi hole";
+    }
+}
---
+++ src/test/java/advent/advent2024/day1/HistorianTester.java
@@ -0,0 +1,15 @@
+package test.java.advent.advent2024.day1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.advent.advent2024.day1.Historian;
+
+public class HistorianTester {
+    @Test
+    void checker(){
+        Historian historian = new Historian();
+        assertEquals(historian.checker(),"hi hole");
+    }
+}
