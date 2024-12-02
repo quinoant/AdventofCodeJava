658793fa2cd22a5f9ff4ca87eeffb89cfaeddf0d
Anthony Quinones <quinoant@gmail.com>
2024-12-02 15:59:10 -0600

    part one

--- src/main/java/advent/advent2024/day2/Nuclear.java
+++ src/main/java/advent/advent2024/day2/Nuclear.java
@@ -5,15 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Nuclear {
-    File file;
-    Scanner sc;
-    // public static void main(String args[])throws FileNotFoundException {
-    //     String input = "/Users/quinoant/Downloads/AdventofCodeJava/src/main/java/advent/advent2024/day2/input.txt";
-    //     String test = "/Users/quinoant/Downloads/AdventofCodeJava/src/main/java/advent/advent2024/day2/test.txt";
-    //     File file = new File(input);
-    //     Scanner sc = new Scanner(file);
-    //     System.out.println("x");
-    // }
+    public File file;
+    public Scanner sc;
+    public static void main(String args[])throws FileNotFoundException {
+        String input = "/Users/quinoant/Downloads/AdventofCodeJava/src/main/java/advent/advent2024/day2/input.txt";
+        String test = "/Users/quinoant/Downloads/AdventofCodeJava/src/main/java/advent/advent2024/day2/test.txt";
+        File file = new File(test);
+        Scanner sc = new Scanner(file);
+        //System.out.println("x");
+    }
 
     public Nuclear(boolean test) throws FileNotFoundException{
         if(test){
@@ -22,6 +22,82 @@ public class Nuclear {
             this.file = new File("src/main/java/advent/advent2024/day2/input.txt");
         }
         sc = new Scanner(file);
+    }
+    public int SafteyCheck(String[] report){
+        int currLevel = Integer.parseInt(report[0]);
+        boolean up = (currLevel - (Integer.parseInt(report[1]))) < 0;
+        if(currLevel - (Integer.parseInt(report[1])) == 0){
+            return 0;
+        }
+        for(int i = 1; i < report.length;i++){
+            int diff = currLevel - Integer.parseInt(report[i]);
+            if(up){
+                if(diff < -3 || diff > -1 ){
+                    return 0;
+                }
+            }else{
+                if(diff < 1 || diff > 3){
+                    return 0;
+                }
+            }
+            currLevel = Integer.parseInt(report[i]);
+        }
+        return 1;
+    }
+    public int SafteyCheckWithDampner(String[] report){
+        int start = 1;
+        boolean damp = true;
+        int currLevel = Integer.parseInt(report[0]);
+        if((currLevel - (Integer.parseInt(report[1]))) == 0){
+            damp = false;
+            start = 2;
+        }
+        boolean up = (currLevel - (Integer.parseInt(report[start]))) < 0;
+        for(int i = start; i < report.length;i++){
+            int diff = currLevel - Integer.parseInt(report[i]);
+            if(up){
+                if(diff < -3 || diff > -1 ){
+                    if(damp){
+                        damp = false;
+                        continue;
+                    }
+                    if(i == 3 && diff > 1 && diff < 3){
+                        up = ((Integer.parseInt(report[0])) - (Integer.parseInt(report[2]))) < 0;
+                        continue;
+                    }
+                    return 0;
+                }
+            }else{
+                if(diff < 1 || diff > 3){
+                    if(damp){
+                        damp = false;
+                        continue;
+                    }
+                    if(i == 3 && diff > -3 && diff < -1){
+                        up = ((Integer.parseInt(report[0])) - (Integer.parseInt(report[2]))) < 0;
+                        continue;
+                    }
+                    return 0;
+                }
+            }
+            currLevel = Integer.parseInt(report[i]);
+        }
+        return 1;
+    }
+
+    public int LevelCheck(boolean problemDampnerOn){
+        int safe = 0;
+        String[] report;
+        while(sc.hasNextLine()){
+            report = sc.nextLine().split(" ");
+            if(problemDampnerOn){
+                safe += SafteyCheckWithDampner(report);
+            }else{
+                safe += SafteyCheck(report);
+            }
+            
+        }
+        return safe;
     }
 
     
--- src/test/java/advent/advent2024/day2/NuclearTester.java
+++ src/test/java/advent/advent2024/day2/NuclearTester.java
@@ -1,6 +1,7 @@
 package test.java.advent.advent2024.day2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.FileNotFoundException;
 
@@ -12,6 +13,58 @@ public class NuclearTester {
     @Test
     void initialization() throws FileNotFoundException{
         Nuclear nuclear = new Nuclear(true);
+        assertNotNull(nuclear.file);
+    }
 
+    @Test
+    void recieving() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(true);
+        assertEquals(nuclear.sc.nextLine(), "7 6 4 2 1");
+    }
+    @Test
+    void testReport() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(true);
+        //String[] safe = "7 6 4 2 1".split(" ");
+        String[] safeTwo = {"7","6","4","2","1"};
+        //String[] unsafe = "1 2 7 8 9".split(" ");
+
+        assertEquals(1,nuclear.SafteyCheck(safeTwo));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
+    }
+
+    @Test
+    void testReportWithDampner() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(true);
+        String[] safe = {"2","6","5","6"};
+        assertEquals(1,nuclear.SafteyCheckWithDampner(safe));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
+    }
+
+    @Test
+    void testTestData() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(true);
+        assertEquals(2,nuclear.LevelCheck(false));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
+    }
+
+    @Test
+    void testTestDataWithDampner() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(true);
+        assertEquals(4,nuclear.LevelCheck(true));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
+    }
+
+    @Test
+    void testInputData() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(false);
+        assertEquals(359,nuclear.LevelCheck(false));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
+    }
+
+    @Test
+    void testInputDataWithDampner() throws FileNotFoundException{
+        Nuclear nuclear = new Nuclear(false);
+        assertEquals(359,nuclear.LevelCheck(true));
+        //assertEquals(0,nuclear.safteyCheck(unsafe));
     }
 }
